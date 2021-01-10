c1729c4ce2fe112212ff1aab1522cae3cd8f87a9
Jeffrey Reese <Reese@MacBook-Pro.local>
2021-01-10 01:21:31 -0500

    Updated Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+const data = require("./database.json");
+
+app.use(express.json());
+
+app.get("/api/employees", (req, res) => {
+  if (!data) {
+    res.status(404).send("Could not find information");
+  }
+  res.send(data);
+});
+
+app.get("/employees/:id", (req, res) => {
+  const findID = data.workers.find(function (employee) {
+    return parseInt(req.params.id) == employee.id;
+  });
+  if (!findID) {
+    res.status(404).send("Error 404 Page Not Found");
+  }
+  res.send(findID);
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
